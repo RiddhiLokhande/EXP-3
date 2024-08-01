8f22ae1b0834e760cc8ec9eaa743c719ceb51692
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 01:21:38 +0530

    Update EXP 3 A.cpp

--- EXP 3 A.cpp
+++ EXP 3 A.cpp
@@ -1,39 +1,29 @@
-#include <iostream>
+//RIDDHI LOKAHNDE
+//ENTC B2
+//23070123107
+//EXP 3 A
+#include<iostream>
 using namespace std;
-
-struct Point {
-    int x;
-    int y;
-};
-
-int main() {
-    // Arithmetic operators
-    int a = 10, b = 3;
-    cout << "Arithmetic Operators:" << endl;
-    cout << "a + b = " << (a + b) << endl;
-    cout << "a - b = " << (a - b) << endl;
-    cout << "a * b = " << (a * b) << endl;
-    cout << "a / b = " << (a / b) << endl;
-    cout << "a % b = " << (a % b) << endl;
-    
-    // Relational operators
-    cout << "\nRelational Operators:" << endl;
-    cout << "a == b: " << (a == b) << endl;
-    cout << "a != b: " << (a != b) << endl;
-    cout << "a > b: " << (a > b) << endl;
-    cout << "a < b: " << (a < b) << endl;
-    cout << "a >= b: " << (a >= b) << endl;
-    cout << "a <= b: " << (a <= b) << endl;
-    
-    // Logical operators
-    bool x = true, y = false;
-    cout << "\nLogical Operators:" << endl;
-    cout << "x && y: " << (x && y) << endl;
-    cout << "x || y: " << (x || y) << endl;
-    cout << "!x: " << (!x) << endl;
-
-    // Assignment operators
+int main(){
+    int a=5;
+    int b=6;
+    int sum,sub,multiplication,modulus,increment,decrement;
     int c = 10;
+    sum=a+b;
+    sub=a-b;
+    multiplication=a*b;
+    modulus=a%b;
+    increment= ++a;
+    decrement= --a;
+    cout << "\nArithmetic Operators:" << endl;
+    cout << "a + b = " << sum << endl;
+    cout << "a - b = " << sub << endl;
+    cout << "a * b = " << multiplication << endl;
+    cout << "a % b = " << modulus<< endl;
+    cout << "++a = " << increment<< endl;
+    cout << "--a = " << decrement<< endl;
+    
+   
     cout << "\nAssignment Operators:" << endl;
     cout << "c = " << c << endl;
     c += 5;
@@ -56,6 +46,19 @@ int main() {
     cout << "c <<= 1: " << c << endl;
     c >>= 1;
     cout << "c >>= 1: " << c << endl;
-
+    
+    cout << "\nComparison  Operators:" << endl;
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+    
+    cout << "\nLogical Operators:" << endl;
+    cout << "x && y: " << (a<5 && a<10) << endl;
+    cout << "x || y: " << (a<5 || a<11) << endl;
+    cout << "!x: " << !(a<5 && a<10) << endl;
+    
     return 0;
 }
